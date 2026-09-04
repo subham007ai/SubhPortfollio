65e15c3d04a204255b33f148c35cb32a424df4f6
Subham Sarangi <subhamsadangi9@gmail.com>
2026-09-04 23:16:05 +0530

    feat(theme): add FeralUI physics ceiling pull-cord light switch

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "@fontsource/instrument-serif/400.css";
 import "@fontsource/instrument-serif/400-italic.css";
+import "pullcord/pullcord.css";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
